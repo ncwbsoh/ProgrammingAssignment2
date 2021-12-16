c3f5eb0199de0cc0fff1f39f3b766e745d078e9b
ncwbsoh <PruiettJacob@JohnDeere.com>
2021-12-16 14:03:23 -0600

    Initial Cache Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Making a special matrix of functions to cache and retrieve both a matrix 
+## and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-print("test commit changes")
-    #Testing again just to be sure
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
@@ -13,4 +23,19 @@ print("test commit changes")
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
